f5517a6847a59b6466d9d74d348191f4e646e6ca
HeronErin <80652149+HeronErin@users.noreply.github.com>
2024-09-25 14:45:52 +0000

    Build: ()

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -5,15 +5,15 @@ var no_tab_mode = 1;
 var mermaid_enabled = 1;
 var toc_pane_div = "right_pane_content";
 var dir_index_pane_div = "";
-var html_url_prefix = "";
-var CONFIGURED_HTML_URL_PREFIX = "";
+var html_url_prefix = "/OSU-notes";
+var CONFIGURED_HTML_URL_PREFIX = "/OSU-notes";
 var CONFIG_ONLY_SHOW_FOR_MULTIPLE_HEADERS = 1;
 var CONFIG_CLOSE_RIGHT_PANE_IF_EMPTY = 1;
 var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '252430296968336165011409456521297861685'                       // used to check whether the localStorage data is stale
+var gzip_hash = '315974852237517925535729330425597386362'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
@@ -530,7 +530,7 @@ function get_graph_data(){
     return get_html_url_prefix()+'/obs.html/data/graph.json';
 }
 function get_html_url_prefix(){
-    return ''  // this value is replaced by the actual url prefix when compiled
+    return '/OSU-notes'  // this value is replaced by the actual url prefix when compiled
 }
 
 function signal_js_enabled(container){
