853ff88d866456e543ede13a32391d51757d46c8
HeronErin <80652149+HeronErin@users.noreply.github.com>
2024-10-04 13:48:17 +0000

    Build: ()

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '227163934638811637259336115243236975230'                       // used to check whether the localStorage data is stale
+var gzip_hash = '275120007268308348729476331185892013362'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
